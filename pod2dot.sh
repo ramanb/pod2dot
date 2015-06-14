ad3cc4597bbef6e7d2707f877d8af7f00d3cfe1b
braman <raman.bob@gmail.com>
2015-06-14 18:57:02 +1000

    Add a wrapper shell script for running pod2dot.rb

---
+++ pod2dot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+filename=`basename $1`
+fileext=${filename##*.}
+filename=`basename $filename .${fileext}`
+echo "Converting $1 to ${filename}.ps"
+
+# Convert the Podfile.lock to .dot
+ruby ~/bin/pod2dot.rb $1 > $1.dot
+
+# Convert the dot file to ps
+# Preview can open ps file
+fdp -Tps $1.dot -o ${filename}.ps
